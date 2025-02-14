754ae1c42b8af443ba682d2f43a823917d92125c
igorGCFelix <igorfelixbob@gmail.com>
2025-02-14 11:54:04 -0300

    igorGCFelix

--- aula1-hello-world/hello-world.py
+++ aula1-hello-world/hello-world.py
@@ -2,7 +2,7 @@ print("Hello World! Iniciando os estudos com Python!")
 
 # git config --local user.name maxxdiego
 # git config --local user.email diego@email.com.br
-
+      
 # git add. 
 # git commit -m "Hello world em Python"
 # git push
